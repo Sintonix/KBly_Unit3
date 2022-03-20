1686c0a7da5945b5737e0e028a70f0b39ac096ae
Sintonix <kelvinbly01@gmail.com>
2022-03-20 15:55:27 -0700

    123sw

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -6,20 +6,19 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject obsPrefab;
     private Vector3 spawnPos = new Vector3(15, 0, 0);
+    private PlayerMovement playerCtrl;
+
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("SpawnObs", 2, 2);
+        playerCtrl = GameObject.Find("Player").GetComponent<PlayerMovement>();
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     void SpawnObs()
     {
-        Instantiate(obsPrefab, spawnPos, obsPrefab.transform.rotation);
+        if (playerCtrl.gameOver == false)
+        {
+            Instantiate(obsPrefab, spawnPos, obsPrefab.transform.rotation);
+        }
     }
 }
