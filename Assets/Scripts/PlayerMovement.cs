e47a5a63b704d27383f806e3bf1ec72c2dc05974
Sintonix <kelvinbly01@gmail.com>
2022-03-25 16:34:08 -0700

    213
    234

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,22 +10,28 @@ public class PlayerMovement : MonoBehaviour
     private bool onGround = true;
     public bool gameOver = false;
 
+    private Animator animPlayer;
+
     // Start is called before the first frame update
     void Start()
     {
         rbPlayer = GetComponent<Rigidbody>();
         Physics.gravity *= gravityModifer;
+
+        animPlayer = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         bool spaceDown = Input.GetKeyDown(KeyCode.Space);
-        if (spaceDown && onGround)
+        if (spaceDown && onGround && !gameOver)
         {
             // Jump Command
             rbPlayer.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             onGround = false;
+
+            animPlayer.SetTrigger("Jump_trig");
         }
     }
     private void OnCollisionEnter(Collision collision)
@@ -34,11 +40,15 @@ public class PlayerMovement : MonoBehaviour
         {
             onGround = true;
         }
+        //Game over when condition met
         else if (collision.gameObject.CompareTag("Obstacle"))
         {
 
             Debug.Log("Game Over!");
             gameOver = true;
+            animPlayer.SetBool("Death_a", true);
+            animPlayer.SetInteger("DeathType_int", 1);
+
         }
     }
 }
